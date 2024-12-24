ac0de290ac926ddd18b4db28b56e7c0c53d0315f
divyashantkumarCS <divyashant@checkedspot.com>
2024-12-24 23:20:49 +0530

    Corrected: Mongoose connection

--- src/db/index.js
+++ src/db/index.js
@@ -6,7 +6,7 @@ import { DB_NAME } from '../constants.js';
 export const connectDB = async () => {
     try {
         console.log("DB connecting...");
-       const conn = await mongoose.connect(`${process.env.MONGODB_URI}`);
+       const conn = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
         console.log(`DB connected, DB host : ${conn.connection.host}`);
     } catch (error) {
         console.log("MongoDB connection failed : ", error);
