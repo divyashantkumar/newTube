df8780fbcea1128a7325b34b2af7cf206ddab5f4
divyashantkumarCS <divyashant@checkedspot.com>
2024-12-24 23:17:47 +0530

    Transfered: all the middleware logic from index.js to app.js

--- src/app.js
+++ src/app.js
@@ -1,6 +1,42 @@
 import express from "express";
+import cors from 'cors';
+import cookieParser from "cookie-parser";
+import healthcheckRouter from './routes/healthcheck.route.js'
+import userRouter from './routes/user.route.js';
+import { errorHandler } from "./middleware/errors.middleware.js";
+
 
 const app = express();
 
 
-export { app }
+// 1. Enable CORS
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    allowedHeaders: [
+        'Content-Type',
+        'Authorization',
+        'Accept',
+    ],
+    credentials: true,
+}));
+
+//2. Enable body parser
+app.use(express.json({ limit: '1mb' }));
+app.use(express.urlencoded({ extended: true, limit: '50mb' }));
+// Inorder to access cookies in request object, cookieParser() middleware is required (mentioned in express docs)
+app.use(cookieParser());
+
+
+// 3. Enable static files
+app.set('view engine', 'ejs');
+app.set("views", "./views");
+app.use(express.static('public'));
+
+
+// ROUTES
+app.use('/api/v1/healthcheck', healthcheckRouter);
+app.use('/api/v1/user', userRouter);
+
+app.use(errorHandler);
+
+export default app 
--- src/index.js
+++ src/index.js
@@ -1,40 +1,10 @@
-import { app } from "./app.js";
-import express from "express";
-import dotenv from "dotenv";
-import cors from 'cors';
-import cookieParser from "cookie-parser";
-import healthcheckRouter from './routes/healthcheck.route.js';
+import { config } from "dotenv";
+config();
 import { connectDB } from "./db/index.js";
-
-dotenv.config();
+import app from "./app.js";
 
 const PORT = process.env.PORT || 3000;
 
-// 1. Enable CORS
-app.use(cors({
-    origin: process.env.CORS_ORIGIN,
-    allowedHeaders: [
-        'Content-Type',
-        'Authorization',
-        'Accept',
-    ],
-    credentials: true,
-}));
-
-//2. Enable body parser
-app.use(express.json({ limit: '1mb' }));
-app.use(express.urlencoded({ extended: true, limit: '50mb' }));
-// Inorder to access cookies in request object, cookieParser() middleware is required (mentioned in express docs)
-app.use(cookieParser());
-
-// 3. Enable static files
-app.set('view engine', 'ejs');
-app.set("views", "./views");
-app.use(express.static('public'));
-
-
-// ROUTES
-app.use('/api/v1/healthcheck', healthcheckRouter);
 
 
 // START SERVER 
