0bd22d650a1012cdf46f8455bc75a4304be021d8
divyashantkumarCS <divyashant@checkedspot.com>
2024-12-26 15:40:58 +0530

    Updated: deleteFiles function in deleteFile.js

--- src/utils/deleteFile.js
+++ src/utils/deleteFile.js
@@ -1,11 +1,23 @@
 import fs from 'fs';
 
-const deleteFile = (filePath) => {
-    try {
-        fs.unlinkSync(filePath);    // remove the file
-    } catch (err) {
-        console.error(err);
+export async function deleteFiles() {
+    const folderPath = path.join(process.cwd(), 'public', 'uploads');
+
+    if (fs.existsSync(folderPath)) {
+        fs.readdirSync(folderPath).forEach((file, index) => {
+            const curPath = path.join(folderPath, file);
+            fs.unlink(curPath, error => {
+                if (error) {
+                    throw error
+                }
+                console.log(`${file} is deleted`);
+            });
+        });
+        console.log(`All files in ${folderPath} deleted successfully!`);
+        return;
+    } else {
+        console.log(`Folder ${folderPath} does not exist!`);
     }
 }
 
-export { deleteFile }
+export { deleteFiles }
